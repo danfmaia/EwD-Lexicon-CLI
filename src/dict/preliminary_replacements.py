275a9bbd75a82a92058bea43f8dabdbbf92aded5
Dan Maia <danilofmaia@gmail.com>
2024-02-13 01:10:24 -0300

    phone update

--- src/dict/preliminary_replacements.py
+++ src/dict/preliminary_replacements.py
@@ -4,17 +4,17 @@ from typing import Dict, List
 
 # TODO: add L2, L3 and FM words (only L1 words are correct)
 PRELIMINARY_REPLACEMENTS: Dict[str, List[str]] = {
-    'the': ['the̬', 'the̬', 'the̬', 'the̬'],
-    'a': ['a̬', 'a̬', 'a̬', 'a̬'],
-    'an': ['a̬n', 'a̬n', 'a̬n', 'a̬n'],
-    'of': ['o̬f', 'o̬f', 'of', 'o̬‹v›'],  # OK
-    'to': ['to̬', 'to̬', 'to̬', 'to̬'],
+    'the': ['the̬', 'the̬', 'the', 'the̬'],
+    'a': ['a̬', 'a̬', 'a', 'a̬'],
+    'an': ['a̬n', 'a̬n', 'an', 'a̬n'],
+    'of': ['o̬‹v›', 'o̬‹v›', 'of', 'o̬‹v›'],
+    'to': ['to̬', 'to̬', 'to', 'to̬'],
     'you': ['yöu', 'yöu', 'yöu', 'yöu'],
     'your': ['yöur', 'yöur', 'yöur', 'yöur'],
-    'this': ['thiṣ', 'thiṣ', 'thiṣ', 'thiṣ'],
+    'this': ['this̩', 'this̩', 'this̩', 'this̩'],
     'and': ['and', 'and', 'and', 'and'],
     'for': ['for', 'for', 'for', 'for'],
-    'from': ['fro̬m', 'fro̬m', 'fro̬m', 'fro̬m'],
+    'from': ['fro̬m', 'fro̬m', 'from', 'fro̬m'],
     # alphabetical order
     'been': ['be͝en', 'be͝en', 'be͝en', 'be͝en'],
 }
